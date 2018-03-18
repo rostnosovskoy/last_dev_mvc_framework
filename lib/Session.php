137ec1eea5567bd565d874d3b90e85989f07b5da
Rost <rostnosovskoy@gmail.com>
2018-03-18 20:47:22 +0200

    Add session and extend db functions

---
+++ lib/Session.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ростислав
+ * Date: 18.03.2018
+ * Time: 16:41
+ */
+
+class Session
+{
+
+}
---
+++ models/Message.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ростислав
+ * Date: 18.03.2018
+ * Time: 17:01
+ */
+
+class Message
+{
+
+}
